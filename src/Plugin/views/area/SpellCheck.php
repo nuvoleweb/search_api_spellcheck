90d5c459b58ee84da86489e680b350e70ba8b94b
Harings Rob <haringsrob@gmail.com>
2017-03-02 12:38:11 +0100

    Initial port

---
+++ src/Plugin/views/area/SpellCheck.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Drupal\search_api_spellcheck\Plugin\views\area;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\search_api\Plugin\views\filter\SearchApiFulltext;
+use Drupal\views\Plugin\views\area\AreaPluginBase;
+
+/**
+ * Provides an area for messages.
+ *
+ * @ingroup views_area_handlers
+ *
+ * @ViewsArea("search_api_spellcheck")
+ */
+class SpellCheck extends AreaPluginBase {
+
+  /**
+   * The available filters for the current view.
+   *
+   * @var array
+   */
+  private $filters;
+
+  /**
+   * The current query parameters.
+   *
+   * @var array
+   */
+  private $currentQuery;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    parent::buildOptionsForm($form, $form_state);
+
+    $form['search_api_spellcheck_title'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('The title to announce the suggestions. Default: suggestions:'),
+      '#default_value' => isset($this->options['search_api_spellcheck_title']) ? $this->options['search_api_spellcheck_title'] : '',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preQuery() {
+    /** @var \Drupal\search_api\Plugin\views\query\SearchApiQuery $query */
+    $query = $this->query;
+    $query->setOption('search_api_spellcheck', TRUE);
+    parent::preQuery();
+  }
+
+  /**
+   * Render the area.
+   *
+   * @param bool $empty
+   *   (optional) Indicator if view result is empty or not. Defaults to FALSE.
+   *
+   * @return array
+   *   In any case we need a valid Drupal render array to return.
+   */
+  public function render($empty = FALSE) {
+    $result = $this->query->getSearchApiResults();
+    // Check if extraData is there.
+    if ($extra_data = $result->getExtraData('search_api_solr_response')) {
+      // Initialize our array.
+      $suggestions = [];
+      // Check that we have suggestions.
+      if (!empty($extra_data['spellcheck'])) {
+        // Loop over the suggestions and print them as links.
+        foreach ($extra_data['spellcheck'] as $suggestion) {
+          // If we have a match within our filters we add the suggestion.
+          if ($filter = $this->getFilterMatch($suggestion)) {
+            $suggestions[] = [
+              '#type' => 'link',
+              '#title' => reset($filter),
+              '#url' => Url::fromRoute('<current>', [], ['query' => array_merge($this->getCurrentQuery(), $filter)]),
+            ];
+          }
+        }
+      }
+      if (!empty($suggestions)) {
+        return [
+          '#title' => $this->getSuggestionLabel(),
+          '#theme' => 'item_list',
+          '#items' => $suggestions,
+        ];
+      }
+    }
+    return [];
+  }
+
+  /**
+   * Gets the current query parameters.
+   *
+   * @return array
+   *   Key value of parameters.
+   */
+  protected function getCurrentQuery() {
+    if (NULL !== $this->currentQuery) {
+      $this->currentQuery = \Drupal::request()->query->all();
+    }
+    return $this->currentQuery;
+  }
+
+  /**
+   * Gets a list of filters.
+   *
+   * @return array
+   *   The filters by key value.
+   */
+  protected function getFilters() {
+    if (NULL === $this->filters) {
+      $this->filters = [];
+      $exposed_input = $this->view->getExposedInput();
+      foreach ($this->view->filter as $key => $filter) {
+        if ($filter instanceof SearchApiFulltext) {
+          $this->filters[$key] = !empty($exposed_input[$key]) ? $exposed_input[$key] : FALSE;
+        }
+      }
+    }
+    return $this->filters;
+  }
+
+  /**
+   * Gets the matching filter for the suggestion.
+   *
+   * @param array $suggestion
+   *   The suggestion array.
+   *
+   * @return bool|string
+   *   False or the matching filter.
+   */
+  private function getFilterMatch($suggestion) {
+    if ($index = array_search($suggestion[0], $this->getFilters(), TRUE)) {
+      // @todo: Better validation.
+      if (!empty($suggestion[1]['suggestion'][0])) {
+        return [$index => $suggestion[1]['suggestion'][0]];
+      }
+    }
+  }
+
+  /**
+   * Gets the suggestion label.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   *   The suggestion label translated.
+   */
+  private function getSuggestionLabel() {
+    return !empty($this->options['search_api_spellcheck_title']) ? $this->options['search_api_spellcheck_title'] : $this->t('Suggestions:');
+  }
+}
